be2fc3adfdd58d007542cd8bd50d291c568348d3
Ugur Timurcin <utimurcin@outlook.com>
2021-02-17 17:40:01 +0300

    BrandApi service and manager created

---
+++ RentACar.FrontEnd/ApiServices/Abstract/IBrandApiService.cs
@@ -0,0 +1,13 @@
+﻿using RentACar.FrontEnd.Models.Brand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentACar.FrontEnd.ApiServices.Abstract
+{
+    public interface IBrandApiService
+    {
+        Task<List<BrandListModel>> GetAllAsync();
+    }
+}
---
+++ RentACar.FrontEnd/ApiServices/Concrete/BrandApiManager.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RentACar.FrontEnd.ApiServices.Abstract;
+using RentACar.FrontEnd.Models.Brand;
+using RentACar.FrontEnd.Models.Car;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RentACar.FrontEnd.ApiServices.Concrete
+{
+    public class BrandApiManager : IBrandApiService
+    {
+        private readonly HttpClient _httpClient;
+        public BrandApiManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:53224/api/brands/");
+        }
+        public async Task<List<BrandListModel>> GetAllAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync("getall");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<List<BrandListModel>>(await responseMessage.Content.ReadAsStringAsync());
+            }
+            return null;
+        }
+    }
+}

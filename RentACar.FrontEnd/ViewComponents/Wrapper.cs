0107ccf85ad13b461557c1dae8631b673316ffb9
Ugur Timurcin <utimurcin@outlook.com>
2021-02-17 17:41:09 +0300

    Component created

---
+++ RentACar.FrontEnd/ViewComponents/Wrapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.FrontEnd.ApiServices.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentACar.FrontEnd.ViewComponents
+{
+    public class Wrapper : ViewComponent
+    {
+        private readonly IBrandApiService _brandApiService;
+        public Wrapper(IBrandApiService brandApiService)
+        {
+            _brandApiService = brandApiService;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            return View(_brandApiService.GetAllAsync().Result);
+        }
+    }
+}
